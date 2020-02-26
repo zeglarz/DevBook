fac2cbcaebca87ad0d323adf969029e4541ab87a
Konrad Rudnicki <zeglarz@gmail.com>
2020-02-26 02:26:03 +0100

    user registration backend complete

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
