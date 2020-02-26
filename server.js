24f688671d9b8fd1ce2081e383c0d6f96486efe5
Konrad Rudnicki <zeglarz@gmail.com>
2020-02-26 01:03:13 +0100

    routes init

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+
+//Connect DB
+
+connectDB();
+
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
@@ -9,3 +15,11 @@ app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 app.get('/', (req, res) => {
     res.send('API Running');
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
