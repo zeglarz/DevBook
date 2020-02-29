e1d171e549a3b54a1080436a2b1e7be6318da1a9
Konrad Rudnicki <zeglarz@gmail.com>
2020-02-29 01:31:50 +0100

    get all posts and post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,8 +6,8 @@ const Post = require('../../models/Post');
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
 
-// @route   GET api/posts
-// @desc    Test route
+// @route   POST api/posts
+// @desc    Add post
 // @access  Private
 
 const createPost = async (req, res) => {
@@ -36,5 +36,45 @@ const createPost = async (req, res) => {
 router.post('/', [auth, [
     check('text', 'Text is required').not().isEmpty()
 ]], createPost);
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+
+const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+
+        if (!posts) return res.status(400).json({ msg: 'No posts' });
+        await res.json(posts);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+router.get('/', auth, getPosts);
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private
+
+const getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) return res.status(400).json({ msg: 'No post with such id' });
+        await res.json(post);
+
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') return res.status(400).json({ msg: 'No post with such id' });
+        res.status(500).send('Server Error');
+    }
+};
+
+router.get('/:id', auth, getPost);
+
 
 module.exports = router;
