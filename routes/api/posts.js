413afbe186ecab1ffb17f9aa993a16bb8a8c0dfb
Konrad Rudnicki <zeglarz@gmail.com>
2020-02-29 01:11:45 +0100

    posting posts add

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
 // @route   GET api/posts
 // @desc    Test route
-// @access  Public
+// @access  Private
 
-router.get('/', (req, res) => res.send('Posts route'));
+const createPost = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+        const post = await newPost.save();
+
+        await res.json(post);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], createPost);
 
 module.exports = router;
