1914cca629d324ecc9612056e3d3e7e9e64afe2b
Konrad Rudnicki <zeglarz@gmail.com>
2020-03-13 23:49:18 +0100

    start to build frontend with react

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Landing(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Landing;
---
+++ client/src/components/layout/Navar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navar(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Navar;
