f41838e7f9420cefcccfb1be5e090396f0bbe5d8
Konrad Rudnicki <zeglarz@gmail.com>
2020-03-16 21:29:59 +0100

    show posts on front

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
@@ -13,7 +14,15 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         <Spinner/>
     ) : (
         <Fragment>
-
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i>
+                Welcome to the DevBook Community
+            </p>
+            {/*  PostForm */}
+            {posts.map(post => (
+                <PostItem key={post._id} post={post}/>
+            ))}
         </Fragment>
     );
 };
